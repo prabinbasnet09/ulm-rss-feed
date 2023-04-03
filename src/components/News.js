411d994bf0f398b7fe7ee92e7366e67107bc2ae5
Prabin Basnet <prabinbasnet09@gmail.com>
2023-04-03 03:19:50 -0500

    Edited rss-feed

--- src/components/News.js
+++ src/components/News.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import parseString from 'xml2js';
 import Pagination from './Pagination';
 
-const ITEMS_PER_PAGE = 4;
+const ITEMS_PER_PAGE = 3;
 
 const paginate = (array, page_size, page_number) => {
   return array.slice((page_number - 1) * page_size, page_number * page_size);
@@ -12,6 +12,39 @@ const paginate = (array, page_size, page_number) => {
 const News = () => {
   const [newsItems, setNewsItems] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
+
+  const formatDate = dateString => {
+    const days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    const date = new Date(dateString);
+    const day = days[date.getDay()];
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    const dateNumber = date.getDate();
+
+    return `${day}, ${dateNumber} ${month} ${year}`;
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,7 +68,11 @@ const News = () => {
 
   return (
     <div className='news-body'>
-      <h1>ULM News</h1>
+      <link
+        href='https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap'
+        rel='stylesheet'
+      />
+      <p className='title'>Breaking News</p>
       <div className='news-grid'>
         {newsItems ? (
           paginate(newsItems, ITEMS_PER_PAGE, currentPage).map(
@@ -47,9 +84,21 @@ const News = () => {
                 />
                 <h2>{item.title}</h2>
                 <p>{item.description}</p>
-                <a href={item.link} target='_blank' rel='noopener noreferrer'>
-                  Full Coverage
+                <a
+                  href={item.link}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='full-coverage'
+                >
+                  Full Coverage &raquo;
                 </a>
+                <div className='flex items-center mt-5 '>
+                  <p className=' font-bold '>By ULM</p>
+                  <p className='ml-2'>|</p>
+                  <p className='ml-2 ' style={{ color: '#840029' }}>
+                    {formatDate(item.pubDate)}
+                  </p>
+                </div>
               </div>
             )
           )
@@ -65,106 +114,102 @@ const News = () => {
           onPageChange={page => setCurrentPage(page)}
         />
       )}
-
-      {/* <style jsx>{`
-        .news-grid {
-          display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-          gap: 1rem;
-        }
-
-        .news-card {
-          background-color: #f8f8f8;
-          padding: 1rem;
-          border-radius: 0.5rem;
-          box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-        }
-
-        .news-card img {
-          max-width: 100%;
-          height: auto;
-          border-radius: 0.5rem;
-        }
-
-        .news-card h2 {
-          font-size: 1.5rem;
-          margin: 1rem 0;
-        }
-
-        .news-card p {
-          font-size: 1rem;
-        }
-
-        .news-card a {
-          display: inline-block;
-          margin-top: 1rem;
-          text-decoration: none;
-          color: #2d7ff9;
-        }
-      `}</style> */}
-
-      <style jsx>{`
-        .news-body {
-          font-family: 'Georgia', serif;
-          background-color: #f3dfc1;
-        }
-
-        .news-grid {
-          display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-          gap: 1rem;
-          padding: 1rem;
-        }
-
-        .news-card {
-          background-color: #fff;
-          padding: 1rem;
-          border-radius: 0.5rem;
-          box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-          position: relative;
-        }
-
-        .news-card::before {
-          content: '';
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          background-image: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 5 5"%3E%3Cpath fill="%23ccc" d="M4 0h1v1H4zm0 2h1v1H4zm0 2h1v1H4zM2 0h1v1H2zm0 2h1v1H2zm0 2h1v1H2zm-2 0h1v1H0zm0-2h1v1H0zm0-2h1v1H0z"/%3E%3C/svg%3E');
-          opacity: 0.15;
-          z-index: -1;
-        }
-
-        .news-card img {
-          max-width: 100%;
-          height: auto;
-          align-self: center;
-          border-radius: 0.5rem;
-          margin-bottom: 1rem;
-        }
-
-        .news-card h2 {
-          font-size: 1.5rem;
-          margin: 0 0 1rem 0;
-          line-height: 1.2;
-        }
-
-        .news-card p {
-          font-size: 1rem;
-          line-height: 1.4;
-          text-align: justify;
-        }
-
-        .news-card a {
-          display: inline-block;
-          margin-top: 1rem;
-          text-decoration: none;
-          color: #2d7ff9;
-        }
-      `}</style>
     </div>
   );
 };
 
 export default News;
+
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import parseString from 'xml2js';
+// import Pagination from './Pagination';
+
+// const ITEMS_PER_PAGE = 4;
+
+// const paginate = (array, page_size, page_number) => {
+//   return array.slice((page_number - 1) * page_size, page_number * page_size);
+// };
+
+// const News = () => {
+//   const [newsItems, setNewsItems] = useState(null);
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [viewMode, setViewMode] = useState('grid');
+
+//   const toggleViewMode = () => {
+//     setViewMode(viewMode === 'grid' ? 'list' : 'grid');
+//   };
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const response = await axios.get('/api/news', {
+//         responseType: 'text',
+//       });
+//       const xmlData = response.data;
+//       parseString.parseString(xmlData, (err, result) => {
+//         if (err) {
+//           console.error(err);
+//         } else {
+//           const allItems = result.rss.channel[0].item;
+//           const filteredItems = allItems.slice(-12);
+//           setNewsItems(filteredItems);
+//         }
+//       });
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div className='news-body'>
+//       <link
+//         href='https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap'
+//         rel='stylesheet'
+//       />
+//       <p className='title'>Breaking News</p>
+//       <button onClick={toggleViewMode}>
+//         Toggle View Mode ({viewMode === 'grid' ? 'List' : 'Grid'})
+//       </button>
+//       <div className={`news-${viewMode}`}>
+//         {newsItems ? (
+//           paginate(newsItems, ITEMS_PER_PAGE, currentPage).map(
+//             (item, index) => (
+//               <div key={index} className='news-card'>
+//                 <img
+//                   src={item['media:content'][0]['media:thumbnail'][0].$.url}
+//                   alt={item.title}
+//                 />
+//                 <div className='news-card-main'>
+//                   <div className='news-card-content'>
+//                     <h2>{item.title}</h2>
+//                     <p>{item.description}</p>
+//                   </div>
+//                   <a
+//                     href={item.link}
+//                     target='_blank'
+//                     rel='noopener noreferrer'
+//                     className='full-coverage'
+//                   >
+//                     Full Coverage &raquo;
+//                   </a>
+//                 </div>
+//               </div>
+//             )
+//           )
+//         ) : (
+//           <p>Loading...</p>
+//         )}
+//       </div>
+
+//       {newsItems && (
+//         <Pagination
+//           currentPage={currentPage}
+//           totalPages={Math.ceil(newsItems.length / ITEMS_PER_PAGE)}
+//           onPageChange={page => setCurrentPage(page)}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default News;
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,3 +1,114 @@
+// import React from 'react';
+
+// const wrapperStyle = {
+//   width: '100%',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//   marginTop: '20px',
+// };
+
+// const pagerStyle = {
+//   display: 'flex',
+//   alignItems: 'center',
+//   fontWeight: 'bold',
+//   fontSize: '18px',
+// };
+
+// const pagerItemStyle = {
+//   margin: '0 10px',
+//   cursor: 'pointer',
+//   color: '#2f3640',
+//   textDecoration: 'none',
+//   transition: '0.3s',
+// };
+
+// const disabledPagerItemStyle = {
+//   ...pagerItemStyle,
+//   cursor: 'not-allowed',
+//   color: '#ccc',
+// };
+
+// const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+//   const previousPage = () => {
+//     if (currentPage > 1) onPageChange(currentPage - 1);
+//   };
+
+//   const nextPage = () => {
+//     if (currentPage < totalPages) onPageChange(currentPage + 1);
+//   };
+
+//   return (
+//     <div style={wrapperStyle}>
+//       <div style={pagerStyle}>
+//         <button
+//           style={currentPage === 1 ? disabledPagerItemStyle : pagerItemStyle}
+//           onClick={previousPage}
+//           disabled={currentPage === 1}
+//         >
+//           Previous
+//         </button>
+//         <span style={pagerItemStyle}>
+//           {currentPage} / {totalPages}
+//         </span>
+//         <button
+//           style={
+//             currentPage === totalPages ? disabledPagerItemStyle : pagerItemStyle
+//           }
+//           onClick={nextPage}
+//           disabled={currentPage === totalPages}
+//         >
+//           Next
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Pagination;
+
+import React from 'react';
+
+const wrapperStyle = {
+  width: 'fit-content',
+  margin: '50px auto 5px auto',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: '20px',
+  fontFamily: 'Arial, Helvetica, sans-serif',
+  border: '1px solid #ccc',
+  padding: '15px 5px',
+  boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.2)',
+  borderRadius: '20px',
+};
+
+const pagerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  fontWeight: 'bold',
+  fontSize: '18px',
+};
+
+const pagerItemStyle = {
+  margin: '0 10px',
+  cursor: 'pointer',
+  color: 'black',
+  textDecoration: 'none',
+  transition: '0.3s',
+};
+
+const disabledPagerItemStyle = {
+  ...pagerItemStyle,
+  cursor: 'not-allowed',
+  color: '#ccc',
+};
+
+const activePagerItemStyle = {
+  ...pagerItemStyle,
+  backgroundColor: '#ffb74d',
+};
+
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   const previousPage = () => {
     if (currentPage > 1) onPageChange(currentPage - 1);
@@ -7,50 +118,41 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
     if (currentPage < totalPages) onPageChange(currentPage + 1);
   };
 
+  const goToPage = pageNumber => {
+    onPageChange(pageNumber);
+  };
+
+  const pageButtons = Array.from({ length: totalPages }, (_, index) => (
+    <button
+      key={index}
+      style={index + 1 === currentPage ? activePagerItemStyle : pagerItemStyle}
+      onClick={() => goToPage(index + 1)}
+    >
+      {index + 1}
+    </button>
+  ));
+
   return (
-    <div className='pagination'>
-      <button onClick={previousPage} disabled={currentPage === 1}>
-        Previous
-      </button>
-      <span>
-        {currentPage} / {totalPages}
-      </span>
-      <button onClick={nextPage} disabled={currentPage === totalPages}>
-        Next
-      </button>
-
-      <style jsx>{`
-        .pagination {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          margin: 1rem 0;
-        }
-
-        button {
-          background-color: #2d7ff9;
-          color: white;
-          border: none;
-          padding: 0.5rem 1rem;
-          margin: 0 0.5rem;
-          border-radius: 0.5rem;
-          cursor: pointer;
-          transition: background-color 0.2s;
-        }
-
-        button:hover {
-          background-color: #4a8ff5;
-        }
-
-        button:disabled {
-          background-color: #a0c1ff;
-          cursor: not-allowed;
-        }
-
-        span {
-          margin: 0 1rem;
-        }
-      `}</style>
+    <div style={wrapperStyle}>
+      <div style={pagerStyle}>
+        <button
+          style={currentPage === 1 ? disabledPagerItemStyle : pagerItemStyle}
+          onClick={previousPage}
+          disabled={currentPage === 1}
+        >
+          &lt;
+        </button>
+        {pageButtons}
+        <button
+          style={
+            currentPage === totalPages ? disabledPagerItemStyle : pagerItemStyle
+          }
+          onClick={nextPage}
+          disabled={currentPage === totalPages}
+        >
+          &gt;
+        </button>
+      </div>
     </div>
   );
 };
